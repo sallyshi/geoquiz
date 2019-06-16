1d1bd98b7d5614b7955c4bb5079a81e865c3e2af
sallyshi <sally.l.shi@gmail.com>
2019-06-15 21:36:14 -0700

    add buttons

--- app/src/main/java/com/bignerdranch/android/geoquiz/QuizActivity.java
+++ app/src/main/java/com/bignerdranch/android/geoquiz/QuizActivity.java
@@ -2,14 +2,21 @@ package com.bignerdranch.android.geoquiz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 import com.bignerdranch.android.geoquiz.R;
 
 public class QuizActivity extends AppCompatActivity {
 
+    private Button mTrueButton;
+    private Button mFalseButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
+
+        mTrueButton = findViewById(R.id.true_button);
+        mFalseButton = findViewById(R.id.false_button);
     }
 }
